7cc38ebd992be0f66604a36ca717f86e735e5021
1Khizar <khizar12006@gmail.com>
2025-08-15 18:47:26 +0500

    Third

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def download_nltk_data():
         nltk.download('punkt')
         nltk.download('stopwords')
         nltk.download('wordnet')
-        nltk.download('averaged_perceptron_tagger_eng')
+        nltk.download('averaged_perceptron_tagger')
     except:
         pass
 
